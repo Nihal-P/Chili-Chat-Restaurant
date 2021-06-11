595909fe1c1b13ef2896a34f26c69c59f8d8cf96
Nihal-P <pate4360@mylaurier.ca>
2021-06-11 00:05:07 -0500

    changed paragraphs to Lorem ipsum

--- src/components/EventsSec.js
+++ src/components/EventsSec.js
@@ -7,22 +7,22 @@ const EventsSec = () => {
                 <div className="event01 eventItem">
                     <h3 className="title">Karaoke Night</h3>
                     <img src="images/image04.jpg" alt="" className="eventImg"/>
-                    <p className="eventInfo">I'm a paragraph. Click here to add your own text and edit me. I’m a great place for you to tell a story and let your users know a little more about you.</p>
+                    <p className="eventInfo">I'm a paragraph. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse varius rhoncus dignissim. Vestibulum in dui vel magna semper yo.</p>
                 </div>
                 <div className="event02 eventItem">
                     <h3 className="title">Enjoy Our Snacks</h3>
                     <img src="images/image05.jpg" alt="" className="eventImg"/>
-                    <p className="eventInfo">I'm a paragraph. Click here to add your own text and edit me. I’m a great place for you to tell a story and let your users know a little more about you.</p>
+                    <p className="eventInfo">I'm a paragraph. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse varius rhoncus dignissim. Vestibulum in dui vel magna semper yo.</p>
                 </div>
                 <div className="event03 eventItem">
                     <h3 className="title">Celebrate With Us</h3>
                     <img src="images/image06.jpg" alt="" className="eventImg"/>
-                    <p className="eventInfo">I'm a paragraph. Click here to add your own text and edit me. I’m a great place for you to tell a story and let your users know a little more about you.</p>  
+                    <p className="eventInfo">I'm a paragraph. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse varius rhoncus dignissim. Vestibulum in dui vel magna semper yo.</p>  
                 </div>
                 <div className="event04 eventItem">
                     <h3 className="title">Happy Hour</h3>
                     <img src="images/image07.jpg" alt="" className="eventImg"/>
-                    <p className="eventInfo">I'm a paragraph. Click here to add your own text and edit me. I’m a great place for you to tell a story and let your users know a little more about you.</p>
+                    <p className="eventInfo">I'm a paragraph. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse varius rhoncus dignissim. Vestibulum in dui vel magna semper yo.</p>
                 </div>
             </div>
         </div>

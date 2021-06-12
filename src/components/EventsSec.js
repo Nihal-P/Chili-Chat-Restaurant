d6b5dc30b562aea590a74fa317472e6e6d7ac082
Nihal-P <pate4360@mylaurier.ca>
2021-06-12 18:35:36 -0500

    Added a read more link to events

--- src/components/EventsSec.js
+++ src/components/EventsSec.js
@@ -8,22 +8,26 @@ const EventsSec = () => {
                     <h3 className="title">Karaoke Night</h3>
                     <img src="images/image04.jpg" alt="" className="eventImg"/>
                     <p className="eventInfo">I'm a paragraph. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse varius rhoncus dignissim. Vestibulum in dui vel magna semper yo.</p>
+                    <a href="/" className="readMore">Read More</a>
                 </div>
                 <div className="event02 eventItem">
                     <h3 className="title">Enjoy Our Snacks</h3>
                     <img src="images/image05.jpg" alt="" className="eventImg"/>
                     <p className="eventInfo">I'm a paragraph. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse varius rhoncus dignissim. Vestibulum in dui vel magna semper yo.</p>
+                    <a href="/" className="readMore">Read More</a>
                 </div>
                 <div className="event03 eventItem">
                     <h3 className="title">Celebrate With Us</h3>
                     <img src="images/image06.jpg" alt="" className="eventImg"/>
                     <p className="eventInfo">I'm a paragraph. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse varius rhoncus dignissim. Vestibulum in dui vel magna semper yo.</p>  
+                    <a href="/" className="readMore">Read More</a>
                 </div>
                 <div className="event04 eventItem">
                     <h3 className="title">Happy Hour</h3>
                     <img src="images/image07.jpg" alt="" className="eventImg"/>
                     <p className="eventInfo">I'm a paragraph. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse varius rhoncus dignissim. Vestibulum in dui vel magna semper yo.</p>
                 </div>
+                <div className="sectionLongLine"></div>
             </div>
         </div>
      );

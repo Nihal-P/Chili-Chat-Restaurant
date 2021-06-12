1e2e5b902c1345ac382e72f8240abea2817e6964
Nihal-P <pate4360@mylaurier.ca>
2021-06-12 18:36:37 -0500

    Added reservation component

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -1,11 +1,13 @@
 import ImgView from '../ImgView';
 import EventsSec from '../EventsSec';
+import Reservation from '../Reservation';
 
 const Home = () => {
     return ( 
         <div>
             <ImgView/>
             <EventsSec/>
+            <Reservation/>
         </div>
      );
 }
